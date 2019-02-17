85a9d2d27298980b046e1051aa9ef052fa7b7ceb
Jan Fornoff <janfornoff@gmail.com>
2019-02-17 13:37:02 +0100

    CLI setup

---
+++ cmd/create.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new Git context.",
+	Long:  `Adds a new config file in ~/.config/gitctx that can be included in project gitconfig-Files.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("create called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/jfornoff/gitctx/cmd"
+
+func main() {
+	cmd.Execute()
+}
